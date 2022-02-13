801349c98bc4ab8c8d225b1cf2a9baaf00f66e93
Tarunverma504 <vermatarun4305@gmail.com>
2022-02-13 15:19:11 +0530

    orders and reviews backend

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,7 +4,10 @@ const { getProducts,
         newProduct, 
         getSingleProduct, 
         updateProduct, 
-        deleteProduct
+        deleteProduct,
+        createProductReview,
+        getProductReviews,
+        deleteReviews
     } = require("../controllers/productController");
 
 const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
@@ -17,4 +20,8 @@ router.route('/admin/product/:id')
                 .put(isAuthenticatedUser,authorizeRoles('admin'), updateProduct)
                 .delete(isAuthenticatedUser,authorizeRoles('admin'),  deleteProduct);
 
+router.route('/review').put(isAuthenticatedUser, createProductReview);
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews);
+router.route('/reviews').delete(isAuthenticatedUser, deleteReviews);
+ 
 module.exports = router;
